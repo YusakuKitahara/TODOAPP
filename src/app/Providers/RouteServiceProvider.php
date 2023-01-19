e33e3c2f892154001406f2f5860e747bb0412d0f
YusakuKitahara <yusaku.kitahara@asia-quest.jp>
2023-01-19 23:42:04 +0900

    入門Laravelチュートリアルのフォルダ仕様にするために動きだし、indexのみ表示させることができた

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -30,6 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
+                ->namespace($this->namespace)
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 

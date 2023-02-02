77bf22e1880ae9357dbc78b5fb7f47420abbb04c
YusakuKitahara <yusaku.kitahara@asia-quest.jp>
2023-02-02 11:03:57 +0900

    routeの作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,12 +13,9 @@ use App\Http\Controllers\TaskController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/folders/{id}/tasks','App\Http\Controllers\TaskController@index')->name('tasks.index');
 
-// Auth::routes();
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::get('/folders/create', 'FolderController@create');
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

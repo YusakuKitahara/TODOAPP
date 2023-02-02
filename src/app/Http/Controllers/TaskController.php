70e1dd32267da2c8da6766e240ace11647225eec
YusakuKitahara <yusaku.kitahara@asia-quest.jp>
2023-02-02 10:50:21 +0900

    Taskモデルなどの編集 CSSまで適応

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
-//        return 'hello, world.';
-
+        // すべてのフォルダを取得
         $folders = Folder::all();
+
+        // 選択フォルダを取得
+        $current_folder = Folder::find($id);
+
+        // 選択フォルダに紐づくタスクを取得
+        $tasks = $current_folder->tasks()->get();
 
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }

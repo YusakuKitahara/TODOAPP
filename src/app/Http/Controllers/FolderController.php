bdc3c439f765a6ea7e1e5069791a66729d7a05cb
YusakuKitahara <yusaku.kitahara@asia-quest.jp>
2023-02-02 11:21:19 +0900

    FolderController.phpの編集

--- src/app/Http/Controllers/FolderController.php
+++ src/app/Http/Controllers/FolderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class FolderController extends Controller
@@ -9,5 +10,19 @@ class FolderController extends Controller
     public function showCreateForm()
     {
         return view('folders/create');
+    }
+
+    public function create(Request $request){
+        $folder = new Folder();
+
+        //　タイトルに入力値を代入
+        $folder->title = $request->title;
+
+        // インスタンスの状態をデータベースに保存
+        $folder->save();
+
+        return redirect()->route('tasks.index', [
+                'id' => $folder->id,
+        ]);
     }
 }

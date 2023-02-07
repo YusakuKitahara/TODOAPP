60d3e491e6f50cfb34a4d0e6161f812759cbaadb
YusakuKitahara <yusaku.kitahara@asia-quest.jp>
2023-02-07 10:39:49 +0900

    タスクを保存できるようにコントローラーを作成し、routeをつなげた

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Folder;
 use App\Models\Task;
+use App\Http\Requests\CreateTask;
 
 
 class TaskController extends Controller
 {
-    public function index(int $id) {
+    public function index(int $id)
+    {
         // すべてのフォルダを取得
         $folders = Folder::all();
 
@@ -26,10 +28,26 @@ class TaskController extends Controller
         ]);
     }
 
-    public function showCreateForm(int $id) {
-
+    public function showCreateForm(int $id)
+    {
         return view('tasks/create', [
             'folder_id' => $id,
         ]);
     }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
+
 }

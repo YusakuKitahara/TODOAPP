a4704a7cdaee03e07f7a3d2f75318fd9f9bd7e64
YusakuKitahara <yusaku.kitahara@asia-quest.jp>
2023-02-07 14:31:00 +0900

    task/editのブレード作成

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -50,4 +50,13 @@ class TaskController extends Controller
         ]);
     }
 
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
 }
